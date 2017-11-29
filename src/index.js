2b37b9ae8b4da78e32a80cbb845732170e13a7a3
yelo <zhihuzeye@gmail.com>
2017-11-29 18:57:22 +0800

    feat: fork repo

---
+++ src/index.js
@@ -0,0 +1,62 @@
+import { parse } from 'url'
+import { helpers } from '@tinajs/tina'
+
+const wechat = wx
+
+class Router {
+  _tabs = []
+  location = ''
+
+  constructor (config) {
+    if (config && config.tabs) {
+      this._tabs = config.tabs
+    }
+  }
+
+  isTab (location) {
+    return ~this._tabs.indexOf(parse(location.slice(1)).pathname)
+  }
+
+  navigate (location) {
+    if (this.isTab(location)) {
+      return wechat.switchTab({
+        url: location,
+      })
+    }
+    return wechat.navigateTo({
+      url: location,
+    })
+  }
+
+  redirect (location) {
+    if (this.isTab(location)) {
+      return wechat.switchTab({
+        url: location,
+      })
+    }
+    return wechat.redirectTo({
+      url: location,
+    })
+  }
+
+  back () {
+    return wechat.navigateBack()
+  }
+}
+
+export default function createRouterMiddleware (options) {
+  let router = new Router(options)
+
+  return function RouterMiddleware (properties) {
+    function current () {
+      return (this && (this.$route && this.$route.fullPath) || this.route) || getCurrentPages()[0].route
+    }
+
+    return helpers.addHooks(properties, {
+      beforeLoad () {
+        this.$router = router
+        this.$router.location = this::current()
+      }
+    })
+  }
+}
