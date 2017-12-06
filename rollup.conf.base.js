f54898334227ff96f3c8979afa7e6b65cc943b50
yelo <zhihuzeye@gmail.com>
2017-12-06 18:32:41 +0800

    build: update rollup config

--- rollup.conf.base.js
+++ rollup.conf.base.js
@@ -1,17 +1,22 @@
 import commonjs from 'rollup-plugin-commonjs'
 import nodejs from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
+import builtins from 'rollup-plugin-node-builtins'
 
 const cssExportMap = {}
 
 export default {
   input: 'src/index.js',
   plugins: [
+    builtins(),
     babel({
       exclude: ['node_modules/**'],
     }),
     nodejs(),
     commonjs(),
   ],
+  globals: {
+    '@tinajs/tina': 'tina',
+  },
   external: ['@tinajs/tina']
 }
