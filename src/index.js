3dcdb3e1719bc0c6a224045fa5d5bfd8ee750ad9
yelo <zhihuzeye@gmail.com>
2017-12-13 03:11:39 +0800

    fix: pass config from default export

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import route from './route'
 import router from './router'
 
-export default function createRouterMixin () {
+export default function createRouterMixin (config) {
   return [
     route,
-    router(),
+    router(config),
   ]
 }
