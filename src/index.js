c339fb6bbe4262442d12af1ab99e9ffd100c2fa8
yelo <zhihuzeye@gmail.com>
2018-01-25 20:08:02 +0800

    feat: use tina-plugin api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import route from './route'
 import router from './router'
 
-export default function createRouterMixin (config) {
+export function createRouterMixin (config) {
   return [
     route,
     router(config),
   ]
 }
+
+const Plugin = {
+  install ({ Page, Component }, config) {
+    let router = createRouterMixin(config)
+    Page.mixin(router)
+    Component.mixin(router)
+  },
+}
+
+export default Plugin
