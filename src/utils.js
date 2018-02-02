210635ebb2b101ee52401fc8c66e51b05984651c
yelo <zhihuzeye@gmail.com>
2018-02-02 18:32:18 +0800

    feat: add troubleshoot link in parse-tab-from-global fn

--- src/utils.js
+++ src/utils.js
@@ -7,7 +7,10 @@ export const globals = {
 
 export function parseTabsFromGlobal (global) {
   if (!global || !global.__wxConfig) {
-    console.warn('[tina-router]: 自动获取全局配置失败，请确认关闭微信开发者工具的 [ES6 转 ES5] 功能。')
+    console.warn([
+      '[tina-router]: 自动获取全局配置失败，请确认关闭微信开发者工具的 [ES6 转 ES5] 功能。',
+      '详情请参考：https://github.com/tinajs/tina-router#' + encodeURI('无法正确地自动获取底部-tab-列表'),
+    ].join('\n'))
     return
   }
   if (!global.__wxConfig.tabBar || !Array.isArray(global.__wxConfig.tabBar.list)) {
