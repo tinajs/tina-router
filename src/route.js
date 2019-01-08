045a47f69c16a2b7ee6cbdf6a9b19e7b3c71b995
Baran Wang <me@baran.wang>
2019-01-08 21:33:30 +0800

    feat: support object

--- src/route.js
+++ src/route.js
@@ -1,12 +1,16 @@
-import querystring from 'querystring'
 import { encode } from './utils'
 
 export default function $route (options, Model) {
   function install (query) {
     const qs = Object.keys(query).map((key) => `${encode(key)}=${query[key]}`).join('&')
+    Object.keys(query).forEach(key => {
+      try {
+        query[key] = JSON.parse(decodeURIComponent(query[key]))
+      } catch (e) { }
+    })
     this.$route = {
       path: `/${this.route}`,
-      query: querystring.parse(qs),
+      query,
       fullPath: qs ? `/${this.route}?${qs}` : `/${this.route}`,
     }
     this.$log('Route Middleware', 'Ready')
